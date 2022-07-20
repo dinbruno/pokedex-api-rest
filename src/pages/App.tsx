b17676e18dbdd69ca975028117e5fef288414480
Bruno <brunodinosantos@outlook.com>
2022-07-20 09:51:30 -0300

    feat: configurate routes, removed unless items

---
+++ src/pages/App.tsx
@@ -0,0 +1,12 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Home } from "../components/home";
+
+export const App: React.FC = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+};
