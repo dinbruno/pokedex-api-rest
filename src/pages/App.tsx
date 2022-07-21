8240ecac140c386537d2032dc1c8a2ca626bfebf
Bruno <brunodinosantos@outlook.com>
2022-07-21 11:24:33 -0300

    fix: page index redirect

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,17 +1,14 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Home } from "../components/home";
 import { Content } from "../components/PokemonPerfil/content";
-import { useApiContext } from "../contexts/ApiContext";
 
 export const App: React.FC = () => {
-
-  // const {pokemonList} = useApiContext()
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path={`/perfil`} element={<Content />} />
 
+        <Route path="/:name" element={<Content />} />
       </Routes>
     </Router>
   );
